99e3cd2a5c0dd497d8610dca1773c7b01afb3643
Franco Battagion <fran.battagion@gmail.com>
2019-01-24 09:58:15 -0300

    Se fija un alto determinado en caja productos y una propiedad de scroll en caso de overflow. Para barrer la lista en caso que el largo exceda el limite establecido, abajo de esta box se encuentra el cierre de caja. Con opcion de seleccion de fecha y muestra del total en pesos. Falta la logica correspondiente y se agrega un pequeño texto que indica "Funcion todavia no disponible". Por ultimo se agregan :key bindings de v-fors para corregir las alertas de eslint con clave unica de identificacion, formato de texto del componente.

--- src/components/sale/sale.vue
+++ src/components/sale/sale.vue
@@ -1,34 +1,38 @@
 <template>
   <div>
     <div id="sales">
-        <div class="row" style="width: 50%; margin: 0 auto; "><div class="centerhead">
-        <h1 class="h1head">Ventas</h1>
+      <div class="row" style="width: 50%; margin: 0 auto; ">
+        <div class="centerhead">
+          <h1 class="h1head">Ventas</h1>
         </div>
         <img :src="require('@/assets/beerra/moneyy2x.png')" alt class="underh1">
-        
       </div>
-         
     </div>
     <div class="container">
       <div class="row">
-
-<div class="col-md-4"> <!-- GROWLERS-->
-          <div class="card bg-dark" style="position: fixed; width: 359px;">
+        <div class="col-md-4">
+          <!-- GROWLERS-->
+          <div class="card bg-dark limitProduct" style="position: fixed; width: 359px;">
             <div class="card-header bg-dark">
-              <h3 class="tablaHead">Productos</h3>
+              <h3 class="tablaHead" >Productos</h3>
             </div>
             <div class="card-body" style="background: #272727;">
               <template v-if="isGrowlers === true">
                 <table class="table">
                   <thead>
                     <th>Cerveza</th>
-                    <th>Cantidad<small> Litros</small></th>
+                    <th>Cantidad
+                      <small>Litros</small>
+                    </th>
                     <th>Precio</th>
                   </thead>
                   <tbody>
-                    <tr class="trHigh" v-for="growler in growlers">
+                    <tr class="trHigh" v-for="growler in growlers" :key="growler._id">
                       <td class="nowrap" style="max-width: 120px;">{{growler.keg.beer}}</td>
-                      <td>{{growler.quantity}}<span class="litros"> l</span></td>
+                      <td>
+                        {{growler.quantity}}
+                        <span class="litros">l</span>
+                      </td>
                       <td>$ {{growler.price}}</td>
                     </tr>
                   </tbody>
@@ -38,13 +42,18 @@
                 <table class="table">
                   <thead>
                     <th>Cerveza</th>
-                    <th>Cantidad<small> Litros</small></th>
+                    <th>Cantidad
+                      <small>Litros</small>
+                    </th>
                     <th>Precio</th>
                   </thead>
                   <tbody>
-                    <tr class="trHigh" v-for="pint in pints">
+                    <tr class="trHigh" v-for="pint in pints" :key="pint._id">
                       <td class="nowrap" style="max-width: 120px;">{{pint.keg.beer}}</td>
-                      <td>{{pint.quantity}}<span class="litros"> l</span></td>
+                      <td>
+                        {{pint.quantity}}
+                        <span class="litros">l</span>
+                      </td>
                       <td>$ {{pint.price}}</td>
                     </tr>
                   </tbody>
@@ -54,13 +63,18 @@
                 <table class="table">
                   <thead>
                     <th>Cerveza</th>
-                  <th>Cantidad<small> Litros</small></th>
+                    <th>Cantidad
+                      <small>Litros</small>
+                    </th>
                     <th>Precio</th>
                   </thead>
                   <tbody>
-                    <tr class="trHigh" v-for="other in others">
+                    <tr class="trHigh" v-for="other in others" :key="other._id">
                       <td class="nowrap" style="max-width: 100px;">{{other.keg.beer}}</td>
-                      <td>{{other.quantity}}<span class="litros"> l</span></td>
+                      <td>
+                        {{other.quantity}}
+                        <span class="litros">l</span>
+                      </td>
                       <td>$ {{other.price}}</td>
                     </tr>
                   </tbody>
@@ -76,10 +90,13 @@
                     <th>Total</th>
                   </thead>
                   <tbody>
-                    <tr class="trHigh" v-for="bottle in bottles">
+                    <tr class="trHigh" v-for="bottle in bottles" :key="bottle._id">
                       <td class="nowrap" style="max-width: 50px;">{{bottle.bottle.beer}}</td>
                       <td>{{bottle.quantitySaled}}</td>
-                      <td>{{bottle.bottle.size}}<span class="litros"> l</span></td>
+                      <td>
+                        {{bottle.bottle.size}}
+                        <span class="litros">l</span>
+                      </td>
                       <td>$ {{bottle.unitPrice}}</td>
                       <td>$ {{bottle.totalPrice}}</td>
                     </tr>
@@ -94,8 +111,15 @@
                     <th>Precio</th>
                   </thead>
                   <tbody>
-                    <tr class="trHigh" v-for="containerSaled in containers">
-                      <td>{{containerSaled.container.size}}<span class="litros"> l</span></td>
+                    <tr
+                      class="trHigh"
+                      v-for="containerSaled in containers"
+                      :key="containerSaled._id"
+                    >
+                      <td>
+                        {{containerSaled.container.size}}
+                        <span class="litros">l</span>
+                      </td>
                       <td>{{containerSaled.quantitySaled}}</td>
                       <td>$ {{containerSaled.totalPrice}}</td>
                     </tr>
@@ -107,8 +131,22 @@
               </template>
             </div>
           </div>
-        </div>
 
+<div class="card  overload col-md-3" style="background: #272727;">
+  <div class=" " >
+    <h3 class="tablaHead" style="margin-top: 0.45em;">Cierre de caja <small> </small></h3> 
+    
+  </div>
+  
+  <div class="" style=" width: 100% !important; padding: 0 !important; margin-bottom: 0.45em;">
+    <input style="margin-bottom: 0.45em;" type="date" class="form-control fecha" v-model="date" required aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+    <h3 class="text-center badge badge-dark precios" style="background: #2b2b2b; border: 1px solid #444444;">Total: $ 0</h3>
+    <span style="color: gray;"><small>La función todavía no está disponible.</small><i  style="color: gray;" class="material-icons resize">query_builder</i></span>
+  </div>
+</div>
+
+
+        </div>
 
         <div class="col-md-8">
           <div class="card" style="background: #272727; margin-bottom: 3em;">
@@ -117,92 +155,115 @@
             </div>
             <div class="card-body">
               <table class="table">
-                <thead class=""> <!-- should be sticky-->
+                <thead class>
+                  <!-- should be sticky-->
                   <th>Cliente</th>
                   <th>Fecha</th>
                   <th>Total</th>
-                  <th>Formatos <small> growler /pinta /botella /cantidad /envase</small></th>
-               
+                  <th>Formatos
+                    <small>growler /pinta /botella /cantidad /envase</small>
+                  </th>
+
                   <th>Elim.</th>
                 </thead>
                 <tbody>
-                  <tr class="trHigh" v-for="sale in sales">
+                  <tr class="trHigh" v-for="sale in sales" :key="sale._id">
                     <td class="nowrap" style="max-width: 100px;">{{sale.client.name}}</td>
 
                     <td>{{format(sale.date)}}</td>
 
                     <td style="width: 100px;">$ {{sale.totalSale}}</td>
                     <div style="width: 290px;">
-                    <template v-if="Object.keys(sale.growlers).length === 0">
-                      <td>
-                        <button class="btn btn-outline-dark disabled disableBorder btn-sm">
-                          <img class="fix" :src="require('@/assets/growlerlleno.png')" alt="carga">
-                        </button>
-                      </td>
-                    </template>
-                    <template v-else>
-                      <td>
-                        <button class="btn btn-light add disableBorder" v-on:click="getGrowler(sale._id)">
-                          <img :src="require('@/assets/growlerlleno.png')" alt="carga">
-                        </button>
-                      </td>
-                    </template>
-                    <template v-if="Object.keys(sale.pints).length === 0">
-                      <td>
-                        <button class="btn btn-outline-dark disabled disableBorder">
-                          <img :src="require('@/assets/pinta.png')" alt>
-                        </button>
-                      </td>
-                    </template>
-                    <template v-else>
-                      <td>
-                        <button class="btn add btn-light disableBorder" v-on:click="getPints(sale._id)">
-                          <img :src="require('@/assets/pinta.png')" alt>
-                        </button>
-                      </td>
-                    </template>
-                    <template v-if="Object.keys(sale.bottles).length === 0">
-                      <td>
-                        <button class="btn btn-outline-dark disabled disableBorder">
-                          <img :src="require('@/assets/bottle.png')" alt>
-                        </button>
-                      </td>
-                    </template>
-                    <template v-else>
-                      <td>
-                        <button class="btn add btn-light disableBorder" v-on:click="getBottles(sale._id)">
-                          <img :src="require('@/assets/bottle.png')" alt>
-                        </button>
-                      </td>
-                    </template>
-                    <template v-if="Object.keys(sale.others).length === 0">
-                      <td>
-                        <button class="btn btn-outline-dark disabled disableBorder">
-                          <img :src="require('@/assets/other.png')" alt>
-                        </button>
-                      </td>
-                    </template>
-                    <template v-else>
-                      <td>
-                        <button class="btn add btn-light disableBorder" v-on:click="getOther(sale._id)">
-                          <img :src="require('@/assets/other.png')" alt>
-                        </button>
-                      </td>
-                    </template>
-                    <template v-if="Object.keys(sale.containers).length === 0">
-                      <td>
-                        <button class="btn btn-outline-dark disabled disableBorder">
-                          <img :src="require('@/assets/carga2.png')" alt>
-                        </button>
-                      </td>
-                    </template>
-                    <template v-else>
-                      <td>
-                        <button class="btn add btn-light disableBorder" v-on:click="getContainers(sale._id)">
-                          <img :src="require('@/assets/carga2.png')" alt>
-                        </button>
-                      </td>
-                    </template></div>
+                      <template v-if="Object.keys(sale.growlers).length === 0">
+                        <td>
+                          <button class="btn btn-outline-dark disabled disableBorder btn-sm">
+                            <img
+                              class="fix"
+                              :src="require('@/assets/growlerlleno.png')"
+                              alt="carga"
+                            >
+                          </button>
+                        </td>
+                      </template>
+                      <template v-else>
+                        <td>
+                          <button
+                            class="btn btn-light add disableBorder"
+                            v-on:click="getGrowler(sale._id)"
+                          >
+                            <img :src="require('@/assets/growlerlleno.png')" alt="carga">
+                          </button>
+                        </td>
+                      </template>
+                      <template v-if="Object.keys(sale.pints).length === 0">
+                        <td>
+                          <button class="btn btn-outline-dark disabled disableBorder">
+                            <img :src="require('@/assets/pinta.png')" alt>
+                          </button>
+                        </td>
+                      </template>
+                      <template v-else>
+                        <td>
+                          <button
+                            class="btn add btn-light disableBorder"
+                            v-on:click="getPints(sale._id)"
+                          >
+                            <img :src="require('@/assets/pinta.png')" alt>
+                          </button>
+                        </td>
+                      </template>
+                      <template v-if="Object.keys(sale.bottles).length === 0">
+                        <td>
+                          <button class="btn btn-outline-dark disabled disableBorder">
+                            <img :src="require('@/assets/bottle.png')" alt>
+                          </button>
+                        </td>
+                      </template>
+                      <template v-else>
+                        <td>
+                          <button
+                            class="btn add btn-light disableBorder"
+                            v-on:click="getBottles(sale._id)"
+                          >
+                            <img :src="require('@/assets/bottle.png')" alt>
+                          </button>
+                        </td>
+                      </template>
+                      <template v-if="Object.keys(sale.others).length === 0">
+                        <td>
+                          <button class="btn btn-outline-dark disabled disableBorder">
+                            <img :src="require('@/assets/other.png')" alt>
+                          </button>
+                        </td>
+                      </template>
+                      <template v-else>
+                        <td>
+                          <button
+                            class="btn add btn-light disableBorder"
+                            v-on:click="getOther(sale._id)"
+                          >
+                            <img :src="require('@/assets/other.png')" alt>
+                          </button>
+                        </td>
+                      </template>
+                      <template v-if="Object.keys(sale.containers).length === 0">
+                        <td>
+                          <button class="btn btn-outline-dark disabled disableBorder">
+                            <img :src="require('@/assets/carga2.png')" alt>
+                          </button>
+                        </td>
+                      </template>
+                      <template v-else>
+                        <td>
+                          <button
+                            class="btn add btn-light disableBorder"
+                            v-on:click="getContainers(sale._id)"
+                          >
+                            <img :src="require('@/assets/carga2.png')" alt>
+                          </button>
+                        </td>
+                      </template>
+                    </div>
                     <td>
                       <button
                         class="btn btn-outline-danger btn-sm fix disableBorder"
@@ -217,24 +278,27 @@
             </div>
           </div>
         </div>
-        
       </div>
     </div>
-          <modal name="delete" height="auto">
-        <div class="container card bg-dark " style="background: #222 !important;">
-          <div class="">
-            <center>
-              <h5 class="tablaHead" style="margin-bottom: 0.7em;">¿Esta seguro que desea eliminar la venta?</h5></center>
-          </div>
-          
-            <div class="input-group-pretend mb-3">
-              <center>
-                  <button class="btn btn-outline-success"  v-on:click="deleteSale()" >Aceptar</button>
-                  <button class="btn btn-outline-danger" v-on:click="hideModal()">Cancelar</button></center>
-            </div>
-            
+    <modal name="delete" height="auto">
+      <div class="container card bg-dark" style="background: #222 !important;">
+        <div class>
+          <center>
+            <h5
+              class="tablaHead"
+              style="margin-bottom: 0.7em;"
+            >¿Esta seguro que desea eliminar la venta?</h5>
+          </center>
         </div>
-      </modal>
+
+        <div class="input-group-pretend mb-3">
+          <center>
+            <button class="btn btn-outline-success" v-on:click="deleteSale()">Aceptar</button>
+            <button class="btn btn-outline-danger" v-on:click="hideModal()">Cancelar</button>
+          </center>
+        </div>
+      </div>
+    </modal>
   </div>
 </template>
 <script>
@@ -244,7 +308,7 @@ const axios = require("axios");
 export default {
   data() {
     return {
-      idSale:'',
+      idSale: "",
       sales: [],
       growlers: [],
       bottles: [],
@@ -377,21 +441,19 @@ export default {
           console.log(response);
           this.getSales();
           this.notifySucces("Venta", response.data.message);
-          this.hideModal()
+          this.hideModal();
         })
         .catch(err => {
           console.log(err.response);
           this.notifyError("Venta", err.response.data);
         });
     },
-    showModal(idSale){
-      
-      this.idSale = idSale
-        this.$modal.show('delete');
-     
+    showModal(idSale) {
+      this.idSale = idSale;
+      this.$modal.show("delete");
     },
-    hideModal(){
-      this.$modal.hide('delete');
+    hideModal() {
+      this.$modal.hide("delete");
     },
     format(date) {
       if (date) return moment(date).format("DD/MM/YYYY");
@@ -425,22 +487,37 @@ export default {
 };
 </script>
 <style scoped>
-.card-body{
+.card-body {
   border-radius: 0.6em;
 }
 
-.add{
- background: #4b4b4b !important;
- border: 1px solid #616161 !important;
+.add {
+  background: #4b4b4b !important;
+  border: 1px solid #616161 !important;
 }
 
-.add:hover{
+.add:hover {
   background: #fff !important;
   color: #242424 !important;
 }
 
-.disabled{
+.disabled {
   width: 47px;
 }
 
+.overload{
+  position: fixed;
+  top: 40em;
+}
+
+.limitProduct{
+height: 337px;
+overflow: scroll;
+}
+
+.precios{
+  font-size: 28px;
+  font-family: 'Squada One', cursive;
+  color:#FD9326;
+}
 </style>
