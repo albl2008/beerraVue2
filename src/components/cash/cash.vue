567fe5ac3d6d13edbd47e6c5b778bf1186fd6396
mariano <marianobuglio@gmail.com>
2019-02-04 18:51:38 -0300

    Termino caja y dejo de mostrar barriles  pagados en barriles

--- src/components/cash/cash.vue
+++ src/components/cash/cash.vue
@@ -9,7 +9,7 @@
 
     <div class="container">
       <div class="row">
-        <div class="col-lg-3 col-md-6">
+        <div class="col-lg-4 col-md-4">
           <div class="card bg-dark">
             <div class="card-body">
               <div class="stat-widget-five">
@@ -19,7 +19,7 @@
                 <div class="stat-content">
                   <div class="text-left dib">
                     <div class="stat-text">
-                      <span class="count">$ 23569</span>
+                      <span class="count">{{"$ "+ total}}</span>
                     </div>
                     <div class="stat-heading">
                       <span>Total</span>
@@ -31,7 +31,7 @@
           </div>
         </div>
 
-        <div class="col-lg-3 col-md-6">
+        <div class="col-lg-4 col-md-4">
           <div class="card bg-dark">
             <div class="card-body">
               <div class="stat-widget-five">
@@ -41,7 +41,7 @@
                 <div class="stat-content">
                   <div class="text-left dib">
                     <div class="stat-text">
-                      <span class="count">$ 3435</span>
+                      <span class="count">{{'$ ' + totalMonth}}</span>
                     </div>
                     <div class="stat-heading">
                       <span>Este mes</span>
@@ -53,7 +53,7 @@
           </div>
         </div>
 
-        <div class="col-lg-3 col-md-6">
+        <!--<div class="col-lg-3 col-md-6">
           <div class="card bg-dark">
             <div class="card-body">
               <div class="stat-widget-five">
@@ -73,9 +73,9 @@
               </div>
             </div>
           </div>
-        </div>
+        </div>-->
 
-        <div class="col-lg-3 col-md-6">
+        <div class="col-lg-4 col-md-4">
           <div class="card bg-dark">
             <div class="card-body">
               <div class="stat-widget-five">
@@ -85,7 +85,7 @@
                 <div class="stat-content">
                   <div class="text-left dib">
                     <div class="stat-text">
-                      <span class="count">$ 2986</span>
+                      <span class="count">{{'$ '+ totalDay}}</span>
                     </div>
                     <div class="stat-heading">
                       <span>Hoy</span>
@@ -100,6 +100,9 @@
 
       <div class="row" style="margin-top: 2em;">
         <div class="col-md-3" style>
+           <div v-if="errorMessage" class="alert alert-danger" role="alert">
+            {{ errorMessage }}
+          </div>
           <div class="card" style="background: #272727;">
             <div class="card-header bg-dark">
               <h3 class="tablaHead">Ingreso +</h3>
@@ -120,6 +123,9 @@
         </div>
 
         <div class="col-md-3">
+          <div v-if="errorMessageOut" class="alert alert-danger" role="alert">
+            {{ errorMessageOut }}
+          </div>
           <div class="card" style="background: #272727;">
             <div class="card-header bg-dark">
               <h3 class="tablaHead">Egreso -</h3>
@@ -150,23 +156,20 @@
                   <th>Monto</th>
                   <th>Descripción</th>
                   <th>Fecha</th>
-                  <th>Opciones</th>
+                  <th>Eliminar</th>
                 </thead>
                 <tbody>
                   <tr class="trHigh" v-for="inflow in inF">
                     <td>{{inflow.amount}}</td>
                     <td>{{inflow.description}}</td>
                     <td>{{format(inflow.date)}}</td>
-                    <div id="options" class="opacityOut">
+                    <div v-if="!inflow.sale" id="options" class="opacityOut">
                       <td>
-                        <button class="btn btn-outline-danger btn-sm fix disableBorder">
+                        <button v-on:click="removeInflow(inflow._id)" class="btn btn-outline-danger btn-sm fix disableBorder">
                           <i class="material-icons resize">clear</i>
                         </button>
                       </td>
                       <td>
-                        <button class="btn btn-outline-primary btn-sm fix disableBorder">
-                          <i class="material-icons resize">edit</i>
-                        </button>
                       </td>
                     </div>
                   </tr>
@@ -178,22 +181,17 @@
                   <th>Monto</th>
                   <th>Descripción</th>
                   <th>Fecha</th>
-                  <th>Opciones</th>
+                  <th>Elimminar</th>
                 </thead>
                 <tbody>
                   <tr class="trHigh" v-for="outflow in out">
                     <td>{{outflow.amount}}</td>
                     <td>{{outflow.description}}</td>
                     <td>{{format(outflow.date)}}</td>
-                    <div id="options" class="opacityOut">
+                    <div v-if="verifyOptions(outflow)" id="options" class="opacityOut">
                       <td>
-                        <button class="btn btn-outline-danger btn-sm fix disableBorder">
+                        <button v-on:click="removeOutflow(outflow._id)" class="btn btn-outline-danger btn-sm fix disableBorder">
                           <i class="material-icons resize">clear</i>
-                        </button>
-                      </td>
-                      <td>
-                        <button class="btn btn-outline-primary btn-sm fix disableBorder">
-                          <i class="material-icons resize">edit</i>
                         </button>
                       </td>
                     </div>
@@ -209,9 +207,15 @@
 </template>
 
 <script>
+import Vue from 'vue'
+import Joi from 'joi'
 import axios from 'axios'
 import moment from 'moment-timezone'
-
+const InSchema = Joi.object().keys({
+  amount: Joi.number().positive().required(),
+  description: Joi.string().required(),
+  date: Joi.date().required()
+})
 class newInFlow {
   constructor(amount,description,date){
     this.amount = amount,
@@ -234,29 +238,76 @@ export default {
       newInFlow:{},
       newOutFlow:{},
       inF:[],
-      out:[]
+      out:[],
+      total:0,
+      totalMonth:0,
+      totalDay:0,
+      errorMessage:'',
+      errorMessageOut:''
+    
 
+    }
+  },
+  watch:{
+    newInFlow:{
+      handler(){
+        this.errorMessage = ''
+      },
+      deep : true
+    },
+      newOutFlow:{
+      handler(){
+        this.errorMessageOut = ''
+      },
+      deep : true
     }
   },
   created(){
     this.getIN()
     this.getOut()
+    this.totals()
+    this.totalsMonth()
+    this.getTotalDay()
+    //this.getTotalWeek()
   },
   methods:{
     addInFlow(){
+      if(this.validIN()){
         axios({
           method:'POST',
           url: process.env.ROOT_API + 'cash/in',
           data: this.newInFlow,
           headers: {authorization: `Bearer ${localStorage.token}`}
         }).then(res =>{
-            console.log(res)
+            console.log("in",res)
             this.getIN()
+            this.totals()
+            this.totalsMonth()
+            this.getTotalDay()
+            this.newInFlow = {}
+            this.notifySucces("Ingreso",res.data.message)
         }).catch(err => {
-            console.log(err.response.data)
+            this.notifyError("Ingreso", res.data.message)
         })
+      }
+    },
+    removeInflow(id){
+      axios({
+        method:'DELETE',
+        url:  process.env.ROOT_API + `cash/${id}`,
+        headers: {authorization: `Bearer ${localStorage.token}`}
+      }).then(res => {
+            this.getIN()
+            this.totals()
+            this.totalsMonth()
+            this.getTotalDay()
+            this.notifySucces("Ingreso",res.data.message)
+      }).catch(err => {
+            this.notifyError("Ingreso",res.data.message)
+      })
     },
     addOutFlow(){
+      if(this.validOut()){
         axios({
           method:'POST',
           url: process.env.ROOT_API + 'cash/out',
@@ -265,9 +316,34 @@ export default {
         }).then(res =>{
             console.log(res)
             this.getOut()
+            this.totals()
+            this.totalsMonth()
+            this.getTotalDay()
+            this.newOutFlow = {}
+            this.notifySucces("Egreso",res.data.message)
         }).catch(err => {
+          this.notifyError("Egreso",res.data.message)
             console.log(err.response.data)
         })
+      }
+    },
+    removeOutflow(id){
+      console.log("Holaaa")
+      axios({
+        method:'DELETE',
+        url:  process.env.ROOT_API + `cash/out/${id}`,
+        headers: {authorization: `Bearer ${localStorage.token}`}
+      }).then(res => {
+          console.log(res)
+          this.getOut()
+          this.totals()
+          this.totalsMonth()
+          this.getTotalDay()
+          this.notifySucces("Egreso",res.data.message)
+      }).catch(err => {
+          this.notifyError("Egreso",res.data.message)
+          console.log(err.response.data)
+      })
     },
     getIN(){
       axios({
@@ -290,9 +366,128 @@ export default {
         console.log(err.response.data)
       })
     },
+    totals(){
+      axios({
+          url: process.env.ROOT_API + 'cash/total',
+          headers: {authorization: `Bearer ${localStorage.token}`}
+      }).then(res =>{
+        console.log(res)
+        this.total = res.data.total
+      }).catch(err=>{
+        console.log(err.response.data)
+      })
+    },
+     totalsMonth(){
+      axios({
+          url: process.env.ROOT_API + 'cash/month',
+          headers: {authorization: `Bearer ${localStorage.token}`}
+      }).then(res =>{
+        let totalMonthIN = this.getMonthYears(res.data.ins)
+        let totalMonthOut = this.getMonthYears(res.data.outs)
+        this.totalMonth = totalMonthIN - totalMonthOut
+      }).catch(err=>{
+        console.log(err.response.data)
+      })
+    },
+    getMonthYears(array){
+            let newArray = []
+           
+             for (const key of array) {
+
+                 if(new Date().getFullYear() === key._id.year && new Date().getMonth()+1 === key._id.month ){
+                      newArray.push(key.count)     
+                                
+                  }
+           
+          }
+            return newArray
+    },
+    getTotalDay(){
+      axios({
+        method:'POST',
+        url: process.env.ROOT_API + `cash/day`,
+        data:{day: new Date()},
+        headers: { authorization: `Bearer ${localStorage.token}` }
+      }).then(res => {
+        
+        console.log("respuesta dias",res)
+        this.totalDay = res.data.totalDay
+      }).catch(err => {
+        console.log("hola",err.response.data)
+      })
+    },
     format(date) {
       if (date) return moment(date).tz('UTC').format("DD/MM/YYYY");
     },
+    verifyOptions(out){
+      if(out.outflow || out.payment){
+        return false
+      }else{
+        return true
+      }
+    },
+    validIN(){
+            const result = Joi.validate(this.newInFlow,InSchema)
+            if(result.error === null){
+                
+                return true
+            }else{
+                console.log(result.error.message)
+                
+                if(result.error.message.includes('amount')){
+                    this.errorMessage = 'Ingreso un monto valido.'
+                }
+                if(result.error.message.includes('date')){
+                    this.errorMessage = 'La fecha ingresada es incorrecta'
+                }
+                if(result.error.message.includes('date')){
+                  this.errorMessage = 'La descripcion ingresada es incorrecta'
+                }
+            }
+    },
+       validOut(){
+            const result = Joi.validate(this.newOutFlow,InSchema)
+            if(result.error === null){
+                
+                return true
+            }else{
+                console.log(result.error.message)
+                
+                if(result.error.message.includes('amount')){
+                    this.errorMessageOut = 'Ingreso un monto valido.'
+                }
+                if(result.error.message.includes('date')){
+                    this.errorMessageOut = 'La fecha ingresada es incorrecta'
+                }
+                if(result.error.message.includes('date')){
+                  this.errorMessageOut = 'La descripcion ingresada es incorrecta'
+                }
+            }
+    },
+    notifyWarning(title,text){
+   Vue.notify({
+                group: 'foo',
+                type: 'warn',
+                title: title,
+                text: text
+              })
+},
+notifySucces(title,text){
+   Vue.notify({
+                group: 'foo',
+                type: 'success',
+                title: title,
+                text: text
+              })
+},
+notifyError(title,text){
+   Vue.notify({
+                group: 'foo',
+                type: 'error',
+                title: title,
+                text: text
+              })
+},
   }
 }
 </script>
