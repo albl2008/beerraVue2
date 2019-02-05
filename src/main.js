13f5444fbfbe24bae38e998594b3c940a6643694
mariano <marianobuglio@gmail.com>
2019-02-05 16:43:47 -0300

    Cambio mainn.js orden de importacion

--- src/main.js
+++ src/main.js
@@ -1,23 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
 import Notifications from 'vue-notification'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css' 
 import VModal from 'vue-js-modal'
-import persistentState from 'vue-persistent-state'
 import ReactiveStorage from "vue-reactive-localstorage";
 import pdf from 'vue-pdf'
 import { Slide } from 'vue-burger-menu'
 import Chart from 'vue2-frappe'
 import EasySlider from 'vue-easy-slider'
-
+import App from './App'
 
 Vue.use(EasySlider)
 Vue.use(Slide)
-Vue.use(Chart)
 Vue.use(pdf)
 Vue.use(Chart)
 Vue.use(ReactiveStorage,{
